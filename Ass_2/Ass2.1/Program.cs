dda0b82b6614c0285820046064f8b4d198b1bc2d
shubham-shinde-1800 <shubhamdshinde1800@gmail.com>
2025-12-24 18:22:14 +0530

    Ass 2 completed..!

---
+++ Ass_2/Ass2.1/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+
+namespace Ass2._1
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Person() : this(0, "")
+        {
+        }
+
+        public Person(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public virtual void AcceptRecord()
+        {
+            Console.Write("Enter Name: ");
+            Name = Console.ReadLine();
+
+            Console.Write("Enter Age: ");
+            Age = int.Parse(Console.ReadLine());
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine("Name : " + Name);
+            Console.WriteLine("Age  : " + Age);
+        }
+    }
+
+    public class Emp : Person
+    {
+        public int Id { get; set; }
+        public double Salary { get; set; } // Base salary
+
+        public Emp() : this(0, 0)
+        {
+        }
+
+        public Emp(double salary, int id)
+        {
+            Salary = salary;
+            Id = id;
+        }
+
+        public override void AcceptRecord()
+        {
+            base.AcceptRecord();
+
+            Console.Write("Enter Employee Id: ");
+            Id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Base Salary: ");
+            Salary = double.Parse(Console.ReadLine());
+        }
+
+        public virtual double CalSalary()
+        {
+            return Salary;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Emp Id : " + Id);
+            Console.WriteLine("Salary : " + CalSalary());
+        }
+    }
+
+    public class Labor : Emp
+    {
+        public int Hour { get; set; }
+        public int Rate { get; set; }
+
+        public Labor() : this(0, 0)
+        {
+        }
+
+        public Labor(int hour, int rate)
+        {
+            Hour = hour;
+            Rate = rate;
+        }
+
+        public override void AcceptRecord()
+        {
+            base.AcceptRecord();
+
+            Console.Write("Enter Hours: ");
+            Hour = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Rate: ");
+            Rate = int.Parse(Console.ReadLine());
+        }
+
+        public override double CalSalary()
+        {
+            return Hour * Rate;
+        }
+    }
+
+    public class Manager : Emp
+    {
+        public int Bonus { get; set; }
+
+        public Manager() : this(0)
+        {
+        }
+
+        public Manager(int bonus)
+        {
+            Bonus = bonus;
+        }
+
+        public override void AcceptRecord()
+        {
+            base.AcceptRecord();
+
+            Console.Write("Enter Bonus: ");
+            Bonus = int.Parse(Console.ReadLine());
+        }
+
+        public override double CalSalary()
+        {
+            return Salary + Bonus;
+        }
+    }
+
+    public class Salesman : Emp
+    {
+        public int Comm { get; set; }
+
+        public Salesman() : this(0)
+        {
+        }
+
+        public Salesman(int comm)
+        {
+            Comm = comm;
+        }
+
+        public override void AcceptRecord()
+        {
+            base.AcceptRecord();
+
+            Console.Write("Enter Commission: ");
+            Comm = int.Parse(Console.ReadLine());
+        }
+
+        public override double CalSalary()
+        {
+            return Salary + Comm;
+        }
+    }
+
+    public class Clerk : Emp
+    {
+        // No extra salary logic
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person p1 = new Manager();
+            //p1.AcceptRecord();
+            //p1.Display();
+
+            //Console.ReadLine();
+
+            //Person p1 = new Salesman();
+            //p1.AcceptRecord();
+            //p1.Display();
+
+            //Person p1 = new Person();
+            //p1.AcceptRecord();
+            //p1.Display();
+
+            Person p1 = new Emp();
+            p1.AcceptRecord();
+            p1.Display();
+        }
+    }
+}
---
+++ Ass_2/Ass2.2Ans/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass2._2Ans
+{
+
+    public struct Student
+    {
+        private string name;
+        private bool gender;
+        private int age;
+        private int std;
+        private char div;
+        private double marks;
+
+        public Student(string name, bool gender, int age, int std, char div, double marks)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+            this.std = std;
+            this.div = div;
+            this.marks = marks;
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public bool Gender
+            { get { return gender; } set {
+                gender = value; } }
+
+        public int Age
+            { get { return age; } set {
+                age = value; } }
+
+        public int Std
+            { get { return std; } set {
+                std = value; } }
+
+        public char Div
+        {
+            get { return div; }
+            set
+            {
+                div = value;
+            }
+        }
+
+            public double Marks
+            { get { return marks; } set {
+                marks = value; } }
+
+
+        public void AcceptDetails()
+        {
+            Console.Write("Enter Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter Gender (M/F): ");
+            char g = char.Parse(Console.ReadLine());
+            gender = (g == 'M' || g == 'm');
+
+            Console.Write("Enter Age: ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Standard: ");
+            std = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Division: ");
+            div = char.Parse(Console.ReadLine());
+
+            Console.Write("Enter Marks: ");
+            marks = double.Parse(Console.ReadLine());
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine("\n--- Student Details ---");
+            Console.WriteLine("Name     : " + name);
+            Console.WriteLine("Gender   : " + (gender ? "Male" : "Female"));
+            Console.WriteLine("Age      : " + age);
+            Console.WriteLine("Standard : " + std);
+            Console.WriteLine("Division : " + div);
+            Console.WriteLine("Marks    : " + marks);
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.AcceptDetails();
+            student.PrintDetails();
+        }
+    }
+}
